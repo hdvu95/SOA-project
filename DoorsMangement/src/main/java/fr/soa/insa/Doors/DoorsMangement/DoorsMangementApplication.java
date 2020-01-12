50d234bb25ce55b805552e8f11c52d6db0403c75
hdvu95 <32202147+hdvu95@users.noreply.github.com>
2020-01-12 21:56:42 +0100

    Add files via upload

---
+++ DoorsMangement/src/main/java/fr/soa/insa/Doors/DoorsMangement/DoorsMangementApplication.java
@@ -0,0 +1,13 @@
+package fr.soa.insa.Doors.DoorsMangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorsMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoorsMangementApplication.class, args);
+	}
+
+}
---
+++ DoorsMangement/src/test/java/fr/soa/insa/Doors/DoorsMangement/DoorsMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.soa.insa.Doors.DoorsMangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorsMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
